459f502f23af24d8f84bdeeb705379a447244f33
jayabalandevops <jayabalandevops@gmail.com>
2019-10-23 08:23:31 +0530

    added string example

---
+++ StringPrograms/StringExample.java
@@ -0,0 +1,18 @@
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// string example programs
+		String s = "Hello World..!   ";
+		System.out.println(s);
+		String sub = s.substring(4, 10);
+		System.out.println(sub);
+		String strimmed = s.trim();
+		System.out.println(strimmed);
+		System.out.println(s.toUpperCase());
+		char c = s.charAt(6);
+		System.out.println(c);
+		
+	}
+
+}
